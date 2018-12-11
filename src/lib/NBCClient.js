420005b199e92a3f94d7f5c4055a4beadd5b6e86
salzmann <marcus.salzmann@peakwork.com>
2018-12-11 06:06:46 +0100

    m

--- src/lib/NBCClient.js
+++ src/lib/NBCClient.js
@@ -7,32 +7,54 @@ export default class NBCClient {
         }) ;
     }
 
+    transformParams(params) {
+        return params;
+    }
+
     requestList(params, orderBy) {
-        return this.$http.post('list', params)
+        return this.$http.post('list', this.transformParams(params))
                 .then(responseObj => responseObj.data);
     }
 
     requestSearch(params) {
-        return this.$http.post('search', params)
+        return this.$http.post('search', this.transformParams(params))
             .then(responseObj => responseObj.data);
     }
 
     requestHotel(params) {
-        return this.$http.post('hotel', params)
-            .then(responseObj => responseObj.data);
+        return this.$http.post('hotel', this.transformParams(params))
+            .then(responseObj => responseObj.data[0] ? responseObj.data[0] : responseObj.data);
     }
 
     requestTeaserImage(params) {
-        params.show = 'pic,katcode';
-        return this.requestSearch(params).then(response => {
-            return new Promise((resolve, reject) => {
-                if (!response.items || response.items.length === 0) {
-                    reject();
-                }
-                let item = response.items[0];
-                let image = item.Images.Medium.find(image => image.type === 'A') || item.Images.Medium[0];
-                resolve(image.url);
+        params.show = 'pic320,katcode,oc';
+        return this.requestSearch(params)
+            .then(response => {
+                return new Promise((resolve, reject) => {
+                    if (!response.items || response.items.length === 0) {
+                        reject();
+                        return;
+                    }
+                    let item = response.items[0];
+                    let image = item.Images.Medium.find(image => image.type === 'A') || item.Images.Medium[0];
+                    resolve(image.Url);
             });
         });
+    }
+
+    requestImages(params) {
+        params.show = 'pic800,katcode,oc';
+        return this.requestHotel(params)
+            .then(response => {
+                return new Promise((resolve, reject) => {
+                    if (!response.items || response.items.length === 0) {
+                        reject();
+                        return;
+                    }
+                    let item = response.items[0];
+                    let images = item.Images.Medium.map(image => image.url);
+                    resolve(image.Url);
+                });
+            });
     }
 }
